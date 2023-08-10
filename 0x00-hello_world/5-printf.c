3022408b1f9d73859725a6298eebe452f41bc64a
Ayamohamedgo <farhaya003@gmail.com>
2023-08-10 13:25:46 -0700

    Hello, puts

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h> 
+/**
+*main - Aprogramethatprintaline using the printf function 
+*Retern: 0 (Success) 
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n") 
+return (0);
+}
